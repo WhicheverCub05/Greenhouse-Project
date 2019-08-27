940083b90665b5ae18f89fd40a284f7dd32542ee
Alex <44294722+WhicheverCub05@users.noreply.github.com>
2019-08-27 02:17:28 +0100

    Update Raspberry_code.py

--- Raspberry_code.py
+++ Raspberry_code.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python3
+from flask import Flask, render_template    
 import serial  # to capture serial data from arduino
 import time  # for pausing program in-between loop
 import re  # for filtering strings from data list
 
-port = "/dev/ttyACM0"
 
-ser = serial.Serial(port, 9600, timeout =2.1)
+port0 = "/dev/ttyACM0"  # Slow Blinker 
+port1 = "/dev/ttyACM1"  # Fast Blinker 
 
+ser0 = serial.Serial(port0, 9600, timeout =4)
+ser1 = serial.Serial(port1, 9600, timeout =4)
 
 start_dud = "b''"  # these duds are made when starting the program
 blank_dud = ''
@@ -17,8 +21,8 @@ def readLine(ser):
 
 
 def ignoreDud(line):
-    dud_line = ''
-    if len(line) <= 50:
+    dud_line = 'dud'
+    if len(line) >= 40 and '(' not in line:
         return line
     else:
         return dud_line
@@ -32,6 +36,10 @@ def splitData(line):
 def filterData(string):
     string = string.translate({ord(i): None for i in ' '})
     string = string.translate({ord(i): None for i in "brnABCDEFGHIJHLMNOPQRSTUVWXYZ:'\\"})
+    if len(string) > 6:
+        string = string[:6]
+    else:
+        pass
     return string    
 
 
@@ -41,7 +49,7 @@ def parseList(data_list):
         try:
             data_list[i] = float(data_list[i])
         except ValueError:
-            print('parsing error: string in data_list')
+            pass
         
     return data_list
 
@@ -53,39 +61,65 @@ def captureData(ser):
     if line != start_dud and line != blank_dud:
         data_list = splitData(line)
         data_list = parseList(data_list)
-        print(data_list)
-
-
-while True:
-    captureData(ser)
-    time.sleep(2.1)
-
-
-'''
-
-testing the main sequence with one board
-
-while True:
-    start_dud = "b''"
-    blank_dud = ''
-    line = readLine(ser)
-    line = str(line)
-    line = ignoreDud(line)
-    if line != start_dud and line != blank_dud:
-        data_list = splitData(line)
-        data_list = parseList(data_list)
-        print(data_list)
-    time.sleep(2.1)
-'''
-
-'''
-
-this is just another method of filtering strings from integers
-although, it does remove the bullet point from a string,
-making a float into an integer which is not desirable.
-
-def filterData(string):
-    string = re.sub("[^0-9]", "", string)
-    return string
+        return data_list
  
-''' 
+
+def removeOldCharacters(characterList):
+    if len(characterList) > 3:
+        del characterList[3:]
+        return characterList
+    else:
+        return characterList
+    
+ 
+
+app = Flask(__name__)
+@app.route('/')
+
+
+def writeToServer():
+    Arduino_zero_data = captureData(ser0)
+    a0 = Arduino_zero_data
+    removeOldCharacters(a0)
+    
+    Arduino_one_data = captureData(ser1)
+    a1 = Arduino_one_data
+    removeOldCharacters(a1)
+    
+    
+    if a0 is not 'dud' and  not a1 is 'dud':
+        try:
+            smos0 = a0[0]
+            ahum0 = a0[1]
+            atemp0 = a0[2]
+        
+            smos1 = a1[0]
+            ahum1 = a1[1]
+            atemp1 = a1[2]
+            
+        except:
+            smos0 = '-'
+            ahum0 = '-'
+            atemp0 = '-'
+        
+            smos1 = '-'
+            ahum1 = '-'
+            atemp1 = '-'
+
+    templateData = {
+        'title' : 'Greenhouse data',
+        
+        'zero_smos' : smos0,
+        'zero_ahum' : ahum0,
+        'zero_atemp' : atemp0,
+        
+        'one_smos' : smos1,
+        'one_ahum' : ahum1,
+        'one_atemp' : atemp1,
+        }
+    return render_template('index.html', **templateData)
+
+
+while True:
+    app.run(host='192.168.1.104', port=8080, debug=False)
+    time.sleep(4)
